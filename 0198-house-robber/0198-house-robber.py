77d055600923c4565892c4e2246e976bad72ca10
Dhruv Shetty <38829073+Dh2Cool@users.noreply.github.com>
2023-03-15 13:58:10 +0530

    Time: 32 ms (71.64%), Space: 13.9 MB (54.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,12 @@
+from functools import lru_cache
 class Solution:
     
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        if len(nums) == 2:
-            return max(nums[0],nums[1])
-        res = [0 for x in range(len(nums))]
-        res[0] = nums[0]
-        res[1] = nums[1]
-        for i in range(len(nums)):
-            if i+2 < len(nums):
-                res[i+2] = max(res[i+2],res[i]+nums[i+2])
-            if i+3 < len(nums):
-                res[i+3] = max(res[i+3],res[i]+nums[i+3])
-        print(res)
-        return max(res[-1],res[-2])
-            
+        self.nums = nums
+        return self.robber(len(nums)-1)
+    @lru_cache()
+    def robber(self, n) -> int:
+        if n < 0:
+            return 0
+        
+        return max(self.robber(n-2)+self.nums[n], self.robber(n-1))
